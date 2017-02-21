7a183e79fec6aa42f1a8da974ee53f46bc3bd67b
Adi Faidz Abdullah <adifaidzabdullah@gmail.com>
2017-02-21 18:03:43 +0800

    Added calls to publish vue components and assets

--- src/Commands/BaseInstallCommand.php
+++ src/Commands/BaseInstallCommand.php
@@ -12,7 +12,7 @@ class BaseInstallCommand extends Command
 
     protected $description = 'Installs base package';
 
-    protected $type = 'Settings';
+    protected $type = 'Base settings and files';
 
     protected $forced = true;
 
@@ -23,6 +23,28 @@ class BaseInstallCommand extends Command
     }
 
     public function handle(){
+      $this->changeSettings();
+      $this->publishVueComponents();
+      $this->publishAssets();
+      $this->info("{$this->type} installed successfully.");
+      $this->call('clean:all');
+    }
+
+    protected function publishVueComponents(){
+      $this->call('vendor:publish',[
+        '--provider' => 'AdiFaidz\Base\Providers\BaseServiceProvider',
+        '--tag'      => 'vue',
+      ]);
+    }
+
+    protected function publishAssets(){
+      $this->call('vendor:publish',[
+        '--provider' => 'AdiFaidz\Base\Providers\BaseServiceProvider',
+        '--tag'      => 'asset',
+      ]);
+    }
+
+    protected function changeSettings(){
       $settings = $this->getSettings();
 
       foreach ($settings as $name => $setting) {
@@ -33,43 +55,74 @@ class BaseInstallCommand extends Command
             $this->info("Successfully registered $name");
         }
       }
-
-      $this->info("{$this->type} installed successfully.");
-      $this->call('clean:all');
     }
 
     protected function getSettings(){
       return [
         'guard config' => [
-             'path'=> '/config/auth.php',
-             'stub'=> __DIR__ . '/stubs/config/guards.stub',
-             'search' => "'guards' => [",
-             'prefix' => false,
+          'path'=> '/config/auth.php',
+          'stub'=> __DIR__ . '/stubs/config/guards.stub',
+          'search' => "'guards' => [",
+          'prefix' => false,
+          'multiline' => false,
+          'callback' => null,
         ],
-        // 'provider config' => [
-        //      'path' => '/config/auth.php',
-        //      'stub' => __DIR__ . '/stubs/config/providers.stub',
-        //      'search' => "'providers' => [",
-        //      'prefix' => false,
-        // ],
         'password config' => [
-             'path' => '/config/auth.php',
-             'stub' => __DIR__ . '/stubs/config/passwords.stub',
-             'search' => "'passwords' => [",
-             'prefix' => false,
+          'path' => '/config/auth.php',
+          'stub' => __DIR__ . '/stubs/config/passwords.stub',
+          'search' => "'passwords' => [",
+          'prefix' => false,
+          'multiline' => false,
+          'callback' => null,
         ],
         'middleware group' => [
-             'path' => '/app/Http/Kernel.php',
-             'search' => 'protected $middlewareGroups = [',
-             'stub' => __DIR__ . '/stubs/middleware/middlewareGroup.stub',
-             'prefix' => false,
-         ],
+          'path' => '/app/Http/Kernel.php',
+          'search' => 'protected $middlewareGroups = [',
+          'stub' => __DIR__ . '/stubs/middleware/middlewareGroup.stub',
+          'prefix' => false,
+          'multiline' => false,
+          'callback' => null,
+        ],
         'route middleware' => [
-             'path' => '/app/Http/Kernel.php',
-             'search' => 'protected $routeMiddleware = [',
-             'stub' => __DIR__ . '/stubs/middleware/routeMiddleware.stub',
-             'prefix' => false,
-         ],
+          'path' => '/app/Http/Kernel.php',
+          'search' => 'protected $routeMiddleware = [',
+          'stub' => __DIR__ . '/stubs/middleware/routeMiddleware.stub',
+          'prefix' => false,
+          'multiline' => false,
+          'callback' => null,
+        ],
+        'javascript bundle' => [
+          'path' => '/resources/assets/js/app.js',
+          'search' => "require('./bootstrap');",
+          'stub' => __DIR__ . '/stubs/assets/js.stub',
+          'prefix' => false,
+          'multiline' => false,
+          'callback' => null,
+        ],
+        'css bundle' => [
+          'path' => '/resources/assets/sass/app.scss',
+          'search' => '@import "node_modules/bootstrap-sass/assets/stylesheets/bootstrap";',
+          'stub' => __DIR__ . '/stubs/assets/scss.stub',
+          'prefix' => false,
+          'multiline' => false,
+          'callback' => null,
+        ],
+        'webpack.mix' => [
+          'path' => '/webpack.mix.js',
+          'search' => "const { mix } = require('laravel-mix');",
+          'stub' => __DIR__ . '/stubs/assets/mix.stub',
+          'prefix' => false,
+          'multiline' => false,
+          'callback' => null,
+        ],
+        'package.json' => [
+          'path' => '/package.json',
+          'search' => '  "devDependencies": {',
+          'stub' => __DIR__ . '/stubs/assets/package.stub',
+          'prefix' => false,
+          'multiline' => true,
+          'callback' => 'checkPackage',
+        ]
       ];
     }
 
@@ -88,15 +141,49 @@ class BaseInstallCommand extends Command
         $originalContent = $this->filesystem->get($path);
         $content = $this->filesystem->get($setting['stub']);
 
-        if( ! str_contains(trim($originalContent), trim($content))) {
-            if ($setting['prefix']) {
-                $stub = $content . PHP_EOL . $setting['search'];
-            } else {
-                $stub = $setting['search'] . PHP_EOL . $content;
-            }
-            $originalContent = str_replace($setting['search'], $stub, $originalContent);
+        if(!$setting['multiline'])
+          return $this->replaceContentWithStub($originalContent, $content, $setting);
+
+        $lines = array_reverse(preg_split("/\r\n|\n|\r/", $content));
+
+        foreach ($lines as $line) {
+          if(!empty($line))
+            $originalContent = $this->replaceContentWithStub($originalContent, $line, $setting, $setting['callback']);
         }
+
         return $originalContent;
+    }
+
+    protected function replaceContentWithStub($originalContent, $content, $setting, $callback= null){
+      $valid = !str_contains(trim($originalContent), trim($content));
+
+      if($callback !== null){
+        $valid = $valid && $this->{$callback}($originalContent,$content);
+      }
+
+      if($valid) {
+
+          if ($setting['prefix']) {
+              $stub = $content . PHP_EOL . $setting['search'];
+          } else {
+              $stub = $setting['search'] . PHP_EOL . $content;
+          }
+
+          $originalContent = str_replace($setting['search'], $stub, $originalContent);
+      }
+
+      return $originalContent;
+    }
+
+    protected function checkPackage($originalContent, &$content){
+      $packagename = preg_split('/:/', $content)[0];
+      $content .= ",";
+
+      if(str_contains(trim($originalContent), trim($packagename)))
+        return false;
+
+
+      return true;
     }
 
     protected function alreadyExists($path){
