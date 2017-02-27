15412cfe4c8a9a2b8f6b5900781870f0f6af486d
Adi Faidz Abdullah <adifaidzabdullah@gmail.com>
2017-02-27 16:27:19 +0800

    Fixed tag without matching end tag

--- resources/assets/js/components/client/userprofile/Form.vue
+++ resources/assets/js/components/client/userprofile/Form.vue
@@ -14,11 +14,11 @@
     </div>
     <div class="form-group">
       <label for="type">Address</label>
-      <textarea v-model="userprofile.address" class="form-control" type="text" id="address" name="address" placeholder="Address">
+      <textarea v-model="userprofile.address" class="form-control" type="text" id="address" name="address" placeholder="Address"></textarea>
     </div>
     <div class="form-group">
       <label for="type">Date of Birth</label>
-      <datetimepicker v-model="userprofile.dob" input-name="dob" placeholder="Date of Birth">
+      <datetimepicker v-model="userprofile.dob" input-name="dob" placeholder="Date of Birth"></datetimepicker>
     </div>
     <div class="form-group">
       <slot></slot>
