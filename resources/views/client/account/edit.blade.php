2ec2971d7cae39914810ae86095c7b0ecc90f5b2
Adi Faidz Abdullah <adifaidzabdullah@gmail.com>
2017-03-28 16:18:43 +0800

    Fixed tab not set to active in view

--- resources/views/client/account/edit.blade.php
+++ resources/views/client/account/edit.blade.php
@@ -16,8 +16,8 @@
   <div class="box box-primary">
     <div class="box-body">
       <ul class="nav nav-pills nav-stacked col-md-3">
-        <li><a href="{{route('client.account.show', ['id'=> $user->id])}}">Account</a></li>
-        <li class="active"><a href="{{route('client.userprofile.show', ['id'=> $user->userprofile->id])}}">Profile</a></li>
+        <li class="active"><a href="{{route('client.account.show', ['id'=> $user->id])}}">Account</a></li>
+        <li><a href="{{route('client.userprofile.show', ['id'=> $user->userprofile->id])}}">Profile</a></li>
       </ul>
       <div class="col-md-9">
         @include('base::client.account.partial.form', ['route'=> route('client.account.update',['id'=> $user->id]), 'method' => 'put'])
